a2f07814644909cc186b2cd61318b75b63b05fb6
FarrukhRasool <farrukh.rasool@pakeventures.com>
2025-06-10 09:55:28 +0200

    german language support added

---
+++ lib/l10n/app_localizations_de.dart
@@ -0,0 +1,43 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for German (`de`).
+class AppLocalizationsDe extends AppLocalizations {
+  AppLocalizationsDe([String locale = 'de']) : super(locale);
+
+  @override
+  String get filterCampsitesTitle => 'Campsites filtern';
+
+  @override
+  String get priceRangeTitle => 'Preisbereich';
+
+  @override
+  String get closeToWaterTitle => 'Nah am Wasser';
+
+  @override
+  String get campFireAllowedTitle => 'Lagerfeuer erlaubt';
+
+  @override
+  String get speakingLanguageTitle => 'Gesprochene Sprache';
+
+  @override
+  String get resetFiltersLabel => 'Filter zurücksetzen';
+
+  @override
+  String get minLabel => 'Min.';
+
+  @override
+  String get maxLabel => 'Max.';
+
+  @override
+  String get campsitesAppBarTitle => 'Campsites';
+
+  @override
+  String get notNearWaterTitle => 'Nicht nah am Wasser';
+
+  @override
+  String get noCampfireTitle => 'Kein Lagerfeuer';
+}
