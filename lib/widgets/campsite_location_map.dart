630074fa8bdf9b3196f10a4ac55ce79faddf6ee7
FarrukhRasool <farrukh.rasool@pakeventures.com>
2025-06-11 20:03:55 +0200

    map location component moved to separate file

---
+++ lib/widgets/campsite_location_map.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import '../resources/colors.dart';
+import '../resources/text_styles.dart';
+import '../models/campsite.dart';
+
+class CampsiteLocationMap extends StatelessWidget {
+  final double latitude;
+  final double longitude;
+
+  const CampsiteLocationMap({
+    super.key,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: FlutterMap(
+        options: MapOptions(
+          center: LatLng(latitude, longitude),
+          zoom: 13.0,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.campsite_finder',
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: LatLng(latitude, longitude),
+                width: 80,
+                height: 80,
+                child: const Icon(
+                  Icons.location_on,
+                  color: Colors.red,
+                  size: 40,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+} 
